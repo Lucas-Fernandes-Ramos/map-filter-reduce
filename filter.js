191152e68cdffccd1f4f731e9ffd971ae944d940
Lucas Fernandes Ramos <lucas-fernandes87@hotmail.com>
2020-05-04 18:31:44 -0300

    Criando o Array de Pets

---
+++ filter.js
@@ -0,0 +1,25 @@
+//Criando um array de Pets
+ const pets = [
+            {
+                name: 'Rex',
+                type: 'Dog',
+                age:  10
+            },
+            {
+                name: 'Dino',
+                type: 'Cat',
+                age:  10
+            },
+            {
+                name: 'Tuba',
+                type: 'fish',
+                age:  10
+            },
+            {
+                name: 'Pegeot',
+                type: 'Bird',
+                age:  10
+            }
+    ]
+
+    console.log(pets)
